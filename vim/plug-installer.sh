f7a4439a317872b1e8cc3ddff3e9fde4ead0dce8
Luis Quintana <luis10_2003@live.com.mx>
2023-01-30 14:39:03 -0600

    installer vim fixed

--- vim/plug-installer.sh
+++ vim/plug-installer.sh
@@ -4,11 +4,11 @@
 #vim config
 
 #install vim-plug
-sh -c 'curl -flo "${xdg_data_home:-$home/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+sh -c 'curl -flo "${xdg_data_home:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
 https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 #install plugins in vim from terminal
-nvim -c 'pluginstall'
+nvim -c 'PlugInstall'
 
 #coc (auto complete) languages servers
 
@@ -19,6 +19,6 @@ sudo npm i -g npm n
 sudo n lts
 
 #languages servers
-nvim -c 'cocinstall coc-sh coc-pyright coc-java coc-tsserver coc-html coc-clangd coc-phpls'
+nvim -c 'CocInstall coc-sh coc-pyright coc-java coc-tsserver coc-html coc-clangd coc-phpls'
 
 
